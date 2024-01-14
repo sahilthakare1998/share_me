594645802ad90ffc625e702e31a8e8bfc009f1c4
sahilthakare1998 <sahilthakare1998@gmail.com>
2024-01-14 14:08:06 +0530

    pins page and add pins page ready

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Routes, Route, useNavigate} from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
+
+      if(!user) navigate('/login');
+  }, [])
     return (
         <h1 className="text-3xl font-bold">
           <Routes>
